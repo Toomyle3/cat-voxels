2c6d05ef5990d2981846a0bc90b531996ef1939f
Tommyle38 <lehoangminh9938@gmail.com>
2024-09-19 17:18:15 +0700

    update

---
+++ component/voxel-dog-loader.js
@@ -0,0 +1,42 @@
+import { forwardRef } from "react";
+import { Box, Spinner } from "@chakra-ui/react";
+
+export const DogSpinner = () => (
+  <Spinner
+    size="xl"
+    position="absolute"
+    left="50%"
+    top="50%"
+    ml="calc(0px - var(--spinner-size) / 2)"
+    mt="calc(0px - var(--spinner-size))"
+  />
+);
+
+export const DogContainer = forwardRef(({ children }, ref) => (
+  <Box
+    ref={ref}
+    className="voxel-dog"
+    style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      background: "#F1E6DB",
+    }}
+    m="auto"
+    w={[280, 480, 800]}
+    h={[280, 480, 800]}
+    position="relative"
+  >
+    {children}
+  </Box>
+));
+
+const Loader = () => {
+  return (
+    <DogContainer>
+      <DogSpinner />
+    </DogContainer>
+  );
+};
+
+export default Loader;
